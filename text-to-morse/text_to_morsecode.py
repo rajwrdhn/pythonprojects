61237f1d3f19771c0553bff36fd69e0bdd2db260
rajwrdhn <rajwrdhn@gmail.com>
2023-08-02 10:35:34 +0200

    text to morse

---
+++ text-to-morse/text_to_morsecode.py
@@ -0,0 +1,35 @@
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'
+                }
+
+def encrypt(message):
+    morse_msg = ""
+    for letter in message:
+        try:
+            if letter != " ":
+                morse_msg += MORSE_CODE_DICT[letter] + " "
+        except KeyError:
+            morse_msg = "Invalid Key!"
+            
+    return morse_msg
+
+def main():
+    message = input("Enter the message to be encrypted! \n").upper()
+    morse = encrypt(message)
+    print(morse)
+
+if __name__== '__main__':
+    main()
